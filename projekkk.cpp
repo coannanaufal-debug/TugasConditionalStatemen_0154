541f6d462596b79c896c5f4eb49350d2545f76a3
coannanaufal-debug <coanna.naufal@gmail.com>
2026-03-09 21:35:32 +0700

    membuat fungsi 1

---
+++ projekkk.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+// Fungsi 1: Menghitung nilai BMI
+float hitungBMI(float berat, float tinggi) {
+    return berat / (tinggi * tinggi);
