03bb62ce8016782b67466abe3e8a573b15de44a9
coannanaufal-debug <coanna.naufal@gmail.com>
2026-03-09 21:38:31 +0700

    Menyelesaikan Fungsi 2

--- projekkk.cpp
+++ projekkk.cpp
@@ -11,4 +11,10 @@ float hitungBMI(float berat, float tinggi) {
 string cekStatus(float bmi) {
     if (bmi < 18.5) {
         return "Berat Badan Kurang";
-    } else if (bmi < 25) {
+    } else if (bmi < 25) {return "Berat Badan Normal";
+    } else if (bmi < 30) {
+        return "Berat Badan Kelebihan";
+    } else {
+        return "Obesitas";
+    }
+}
